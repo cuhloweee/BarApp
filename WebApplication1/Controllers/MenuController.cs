a54b0a7ae0230ab92d0bf2c919f597953cb7855f
Chloe Cruz <Chloe Cruz>
2017-08-30 22:08:43 -0400

    Add project files.

---
+++ WebApplication1/Controllers/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Menu()
+        {
+            var entities = new MenuEntities1();
+            return View(entities.Menus.ToList());
+
+        }
+
+
+    }
+}
